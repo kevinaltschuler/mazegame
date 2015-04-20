565d0f79339b6f575bb72daf76f83552dede50bd
kevtastic <kevinaltschuler@gmail.com>
2015-04-19 22:32:58 -0400

    some stuff

--- src/MazeWorld.java
+++ src/MazeWorld.java
@@ -19,8 +19,12 @@ import java.awt.Color;
 //Represents an Edge of a Cell
 class Edge implements Comparator<Edge>, Comparable<Edge> {
     Integer weight;
-    Edge()
+    Cell c1;
+    Cell c2;
+    Edge(Cell c1, Cell c2)
     {
+        this.c1 = c1;
+        this.c2 = c2;
         Random r = new Random();
         weight = r.nextInt(100);
     }
@@ -29,6 +33,11 @@ class Edge implements Comparator<Edge>, Comparable<Edge> {
     }
     public int compareTo(Edge arg0) {
         return this.weight.compareTo(arg0.weight);
+    }
+    public boolean sameEdge(Edge that) {
+        return ((this.c1.sameCell(that.c1) && this.c2.sameCell(that.c2)) ||
+                this.c1.sameCell(that.c2) && this.c2.sameCell(that.c1));
+
     }
 }
 // Represents a single square of the game area
@@ -39,9 +48,12 @@ class Cell {
     int y;
     //size in pixels
     static final int SIZE = 650/MazeWorld.WIDTH;
-    Cell(double height, int x, int y) {
+    Cell(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+    public boolean sameCell(Cell that) {
+        return (this.x == that.x && this.y == that.y);
     }
     WorldImage cellImage(double waterHeight) {
         return new RectangleImage(new Posn(this.x, this.y), 
@@ -67,12 +79,41 @@ class MazeWorld extends World {
         //default constructor
     }
     void reset(int width, int height) {
-        int numedges = ((MazeWorld.HEIGHT + 1) * MazeWorld.WIDTH) + ((MazeWorld.WIDTH + 1) * MazeWorld.HEIGHT);
-        for (int i = numedges; i >= 0; i -= 1)
-        {
-            edges.add(new Edge());
+        for (int i = 0; i <= MazeWorld.WIDTH; i += 1) {
+            for (int j = 0; j <= MazeWorld.HEIGHT; j += 1) {
+                board.get(i).add(new Cell(i, j));
+            }
+        }
+        for (int i = 0; i <= MazeWorld.WIDTH; i += 1) {
+            for (int j = 0; j <= MazeWorld.HEIGHT; j += 1) {
+                edges.add(new Edge(board.get(i).get(j), board.get((i + 1) % MazeWorld.WIDTH).get(j)));
+                edges.add(new Edge(board.get(i).get(j), board.get(Math.abs(i - 1)).get(j)));
+                edges.add(new Edge(board.get(i).get(j), board.get(i).get((j + 1) % MazeWorld.HEIGHT)));
+                edges.add(new Edge(board.get(i).get(j), board.get(i).get(Math.abs(j - 1))));
+            }
         }
         Collections.sort(edges);
+        this.removeDuplicates(edges);
+    }
+    public int removeDuplicates(ArrayList<Edge> edges) {
+        if (edges.size() <= 2) {
+            return edges.size();
+        }
+        
+        int prev = 1; // point to previous
+        int curr = 2; // point to current
+
+        while (curr < edges.size()) {
+            if (edges.get(curr).sameEdge(edges.get(prev)) && edges.get(curr).sameEdge(edges.get(prev - 1))) {
+                curr += 1;
+            } else {
+                prev += 1;
+                edges.set(prev, edges.get(curr));
+                curr += 1;
+            }
+        }
+
+        return prev + 1;
     }
     //The entire background image for this world
     public WorldImage background = 
@@ -81,7 +122,7 @@ class MazeWorld extends World {
     public WorldImage makeImage() {
         WorldImage acc = new RectangleImage(new Posn(0, 0), 
                 0, 0, new Black());
-        
+
         return acc;
     }
     void updateCells() {
@@ -118,7 +159,6 @@ class MazeWorld extends World {
 
 class ExamplesWorld {
     //TODO
-    Cell c3 = new Cell(50.0, 20, 10);
     //Cell c4 = new Cell(25.0, 10, 20);
     //Cell c5 = new Cell(25.0, 10, 0);
 
